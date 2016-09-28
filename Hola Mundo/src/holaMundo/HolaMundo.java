2c1e646f73d3e45e030fb8f0abbb321a2d6937f8
alumno <alumno@w701>
2016-09-28 09:18:20 +0200

    Nuevo mensaje desde la máquina principal

---
+++ Hola Mundo/src/holaMundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola mundo");
+		
+		System.out.println("Prueba Git desde máquina principal");
+
+	}
+
+}
