7da047a1906503d24159c1f2080a3043b5b4a752
alumno <alumno@w701>
2016-09-28 09:26:15 +0200

    Nuevo mensaje segunda máquina

--- Hola Mundo/src/holaMundo/HolaMundo.java
+++ Hola Mundo/src/holaMundo/HolaMundo.java
@@ -7,6 +7,8 @@ public class HolaMundo {
 		System.out.println("Hola mundo");
 		
 		System.out.println("Prueba Git desde máquina principal");
+		
+		System.out.println("Prueba Git desde segunda máquina");
 
 	}
 
